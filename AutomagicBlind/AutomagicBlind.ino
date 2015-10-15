a3b755cb72dacba7413562e36a485ba111d6ba9b
Nils Högberg <nils.hogberg@gmail.com>
2015-10-16 00:09:39 +0200

    Added joystick and sleep

--- AutomagicBlind/AutomagicBlind.ino
+++ AutomagicBlind/AutomagicBlind.ino
@@ -3,12 +3,16 @@
 #define DEBUG
 #define STEP_PIN 13
 #define DIR_PIN 12
-#define CW 0
-#define CCW 1
+#define SLEEP_PIN 11
+#define RESET_PIN 9
+#define JOY_PIN1 0
+#define JOY_PIN2 1
+#define CW 1
+#define CCW 0
 
-uint16_t curr_revolutions = 0;
+uint16_t curr_step = 0;
 uint16_t steps_per_rev = 200;
-uint16_t steps_to_down = 10000;
+uint16_t steps_to_down = 500;
 uint8_t step_pin_state = 0;
 
 char			serial_data[101];			// Array for incoming serial-data 
@@ -16,34 +20,59 @@ unsigned char	serial_index = 0;			// How many bytes have been received?
 char			string_started = 0;			// Only saves data if string starts with right byte
 
 enum direction {cw, ccw};
-enum mode {neutral, up, down};
+enum mode {neutral, up, down, sleep};
 mode current_mode = neutral;
 
 unsigned long curr_millis;
+unsigned long curr_us;
+unsigned long prev_step_us = 0;
+unsigned long us_between_steps = 10000;
 unsigned long prev_step_millis = 0;
-unsigned long millis_between_steps = 1; // milliseconds
+unsigned long millis_between_steps = 10; // milliseconds
 
 // Timer 1 interrupt 1Hz
-ISR(TIMER1_COMPA_vect)
+/*ISR(TIMER1_COMPA_vect)
 {
 	
 }
-
+*/
 void stepMotor(uint8_t dir)
 {
 	// Set the direction
-	if (dir == CW)
-	{
-		digitalWrite(DIR_PIN, LOW);
-	}
-	else
-	{
-		digitalWrite(DIR_PIN, HIGH);		
-	}
-
+	digitalWrite(DIR_PIN, dir);
 	// Step motor one step
 	digitalWrite(STEP_PIN, HIGH);
 	digitalWrite(STEP_PIN, LOW);
+}
+
+void handleMovement()
+{
+	if (current_mode == up)
+	{
+		if (curr_step > 0)
+		{
+			stepMotor(CW);
+			curr_step--;
+		}
+		else
+		{
+			toggleSleep();
+			current_mode = sleep;
+		}
+	}
+	else if (current_mode == down)
+	{
+		if (curr_step < steps_to_down)
+		{
+			stepMotor(CCW);
+			curr_step++;
+		}
+		else
+		{
+			toggleSleep();
+			current_mode = sleep;
+		}
+	}
 }
 
 void handleSerialCommands()
@@ -62,6 +91,8 @@ void handleSerialCommands()
 #ifdef DEBUG
 				Serial.println("Received command DOWN.");
 #endif
+				if (current_mode == sleep)
+					toggleSleep();
 				current_mode = down;
 			}
 
@@ -71,6 +102,8 @@ void handleSerialCommands()
 #ifdef DEBUG
 				Serial.println("Received command UP.");
 #endif
+				if (current_mode == sleep)
+					toggleSleep();
 				current_mode = up;
 			}
 			else if (serial_data[serial_index - 4] == 'S' &&
@@ -83,6 +116,37 @@ void handleSerialCommands()
 #endif
 				current_mode = neutral;
 			}
+			else if (serial_data[serial_index - 5] == 'S' &&
+					serial_data[serial_index - 4] == 'L' &&
+					serial_data[serial_index - 3] == 'E' &&
+					serial_data[serial_index - 2] == 'E' &&
+					serial_data[serial_index - 1] == 'P')
+			{
+#ifdef DEBUG
+				Serial.println("Received command SLEEP.");
+#endif
+				if (current_mode != sleep)
+				{
+					toggleSleep();
+					current_mode = sleep;
+				}
+				else
+				{
+					toggleSleep();
+					current_mode = neutral;
+				}
+			}
+			else if (serial_data[serial_index - 5] == 'D' &&
+				serial_data[serial_index - 4] == 'E' &&
+				serial_data[serial_index - 3] == 'B' &&
+				serial_data[serial_index - 2] == 'U' &&
+				serial_data[serial_index - 1] == 'G')
+			{
+#ifdef DEBUG
+				Serial.println("Received command DEBUG.");
+#endif
+				printDebug();
+			}
 		}
 		else if (Serial.read() == '$')
 		{
@@ -91,12 +155,45 @@ void handleSerialCommands()
 	}
 }
 
+void toggleSleep()
+{
+	uint8_t res = digitalRead(SLEEP_PIN);
+
+	if (res == LOW)
+	{
+		digitalWrite(SLEEP_PIN, HIGH);
+	}
+	else
+	{
+		digitalWrite(SLEEP_PIN, LOW);
+	}
+}
+
+void handleJoyStick()
+{
+	uint16_t value = analogRead(JOY_PIN1);
+	Serial.print("value:");
+	Serial.println(value);
+	if ((value > 712) && (curr_step > 0))
+	{
+		stepMotor(CW);
+		curr_step--;
+	}
+	else if ((value < 312) && (curr_step < steps_to_down))
+	{
+		stepMotor(CCW);
+		curr_step++;
+	}
+}
+
 void setup()
 {
 	Serial.begin(19200);
 	pinMode(STEP_PIN, OUTPUT);
 	pinMode(DIR_PIN, OUTPUT);
-	
+	pinMode(SLEEP_PIN, OUTPUT);
+	pinMode(RESET_PIN, OUTPUT);
+	/*
 	cli();		// disable all interrupts
 	// Setup 1 Hz interrupt on timer 1
 	TCCR1A = 0;								// set TCCR1A to 0
@@ -107,15 +204,39 @@ void setup()
 	TCCR1B |= (1 << CS12) | (1 << CS10);	// Set CS10 and CS12 bits for 1024 prescaler
 	TIMSK1 |= (1 << OCIE1A);				// enable timer compare interrupt
 	sei();		// enable all interrupts
-	
+	*/
+	digitalWrite(RESET_PIN, HIGH);
+	digitalWrite(SLEEP_PIN, HIGH);
+	digitalWrite(DIR_PIN, HIGH);
+
+	for (int i = 0; i <= 10; i++)
+	{
+		digitalWrite(STEP_PIN, HIGH);
+		digitalWrite(STEP_PIN, LOW);
+		delay(10);
+	}
+	delay(100);
+	digitalWrite(DIR_PIN, LOW);
+	for (int i = 0; i <= 10; i++)
+	{
+		digitalWrite(STEP_PIN, HIGH);
+		digitalWrite(STEP_PIN, LOW);
+		delay(10);
+	}
 }
 
 void loop()
 {
 	handleSerialCommands();
-
-	curr_millis = millis();
 	
+	curr_millis = millis();
+	if (curr_millis - prev_step_millis >= millis_between_steps)
+	{
+		prev_step_millis += millis_between_steps;
+		handleJoyStick();
+		handleMovement();
+	}
+	/*
 	if (curr_millis - prev_step_millis >= millis_between_steps)
 	{
 		prev_step_millis += millis_between_steps;
@@ -125,10 +246,10 @@ void loop()
 		case neutral:
 			break;
 		case up:
-			if (curr_revolutions > 0)
+			if (curr_step > 0)
 			{
 				stepMotor(CW);
-				curr_revolutions--;
+				curr_step--;
 			}
 			else
 			{
@@ -136,18 +257,48 @@ void loop()
 			}
 			break;
 		case down:
-			if (curr_revolutions < steps_to_down)
+			if (curr_step < steps_to_down)
 			{
 				stepMotor(CCW);
-				curr_revolutions++;
+				curr_step++;
 			}
 			else
 			{
 				current_mode = neutral;
 			}
 			break;
+		case sleep:
+			break;
 		default:
 			break;
 		}
+	}*/
+}
+
+void printDebug()
+{
+	Serial.println("----DEBUG----");
+	Serial.print("curr_step: ");
+	Serial.println(curr_step);
+	Serial.print("steps_to_down: ");
+	Serial.println(steps_to_down);
+	Serial.print("current_mode: ");
+	switch (current_mode)
+	{
+	case neutral:
+		Serial.println("neutral");
+		break;
+	case up:
+		Serial.println("up");
+		break;
+	case down:
+		Serial.println("down");
+		break;
+	case sleep:
+		Serial.println("sleep");
+		break;
+	default:
+		Serial.println("mode not known");
+		break;
 	}
 }
